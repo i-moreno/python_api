653f9da3d96d0b1421a4fb1489f5cf3541c17e53
Carlos <juan.moreno@improving.com>
2022-12-20 17:06:52 -0600

    Create schemas for post and integrate with main file

--- app/main.py
+++ app/main.py
@@ -1,25 +1,18 @@
-from fastapi import FastAPI, HTTPException, Response, status, Depends
-from fastapi.params import Body
+from fastapi import FastAPI, HTTPException, status, Depends
+from typing import List
 
-from typing import Optional
-from pydantic import BaseModel
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 
 from sqlalchemy.orm import Session
 from . import models
-from . database import engine, get_db
+from .schemas import PostBase, PostCreate, PostResponse
+from .database import engine, get_db
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
-
-
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = False
 
 
 # Database connection
@@ -35,16 +28,16 @@ while True:
         time.sleep(4)
 
 
-@app.get("/posts")
+@app.get("/posts", response_model=List[PostResponse])
 async def get_post(db:Session = Depends(get_db)):
   # cursor.execute("""SELECT * FROM posts""")
   # posts = cursor.fetchall()
   posts = db.query(models.Post).all()
-  return {"data": posts}
+  return posts
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-async def create_post(payload: Post, db: Session = Depends(get_db)):
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=PostResponse)
+async def create_post(payload: PostCreate, db: Session = Depends(get_db)):
   # cursor.execute(""" INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, (payload.title, payload.content, payload.published))
   # new_post = cursor.fetchone()
   # ** it's equal to -> title=payload.title, content=payload.content and so on
@@ -52,10 +45,10 @@ async def create_post(payload: Post, db: Session = Depends(get_db)):
   db.add(new_post) # Add the newly created post to db
   db.commit() # Commit changes to db
   db.refresh(new_post) # Retrieve and store the new post in new_post variable
-  return {"data": new_post}
+  return new_post
 
 
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model=PostResponse)
 def get_post(id: int, db: Session = Depends(get_db)):
   # cursor.execute(""" SELECT * from posts WHERE id = %s """, (str(id)))
   # post = cursor.fetchone()
@@ -64,11 +57,11 @@ def get_post(id: int, db: Session = Depends(get_db)):
   if not post:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
 
-  return {"data": post}
+  return post
 
 
-@app.put("/posts/{id}")
-def update_post(id: int, payload: Post, db: Session = Depends(get_db)):
+@app.put("/posts/{id}", response_model=PostResponse)
+def update_post(id: int, payload: PostBase, db: Session = Depends(get_db)):
     # cursor.execute(""" UPDATE posts SET title=%s, content=%s, published=%s WHERE id=%s RETURNING *  """, (payload.title, payload.content, payload.published, str(id)))
     # updated_post = cursor.fetchone()
   post = db.query(models.Post).filter(models.Post.id == id)
@@ -80,7 +73,7 @@ def update_post(id: int, payload: Post, db: Session = Depends(get_db)):
   db.commit()
   update_post = post.first()
 
-  return {"data": update_post}
+  return update_post
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
