5f0af0193793319e41eb4fe5b5c5d063255e8f91
Carlos <juan.moreno@improving.com>
2022-12-21 17:27:30 -0600

    Add login flow

---
+++ app/oauth2.py
@@ -0,0 +1,52 @@
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from . import schemas
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+# SECRET_KEY
+# Algorithm
+# Expiration Time
+
+SECRET_KEY = "959gfjks59s923ka94sls9549s9a9sk49aks90459skd94ks92kd04kas9023kjd"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict):
+  to_encode = data.copy()
+  expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+  # Add expiration time
+  to_encode.update({"exp": expire})
+
+  encoded = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+  return encoded
+
+
+def verify_access_token(token: str, credentials_exception):
+  try:
+    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    id: str = payload.get("user_id")
+
+    if not id:
+      raise credentials_exception
+
+    token_data = schemas.TokenData(id=id)
+  except JWTError as e:
+    print(e)
+    raise credentials_exception
+
+  return token_data
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+  credentials_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail=f"Could not validate credentials",
+    headers={"WWWW-Authenticate":"Bearer"}
+    )
+
+  return verify_access_token(token, credentials_exception)
---
+++ app/routers/auth.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+
+from ..database import get_db
+from ..schemas import UserLogin
+from .. import models, utils, oauth2
+
+router = APIRouter(tags=["Authentication"])
+
+@router.post("/login")
+def login(payload: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+  user = db.query(models.User).filter(models.User.email == payload.username).first()
+
+  if not user:
+    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid credentials")
+
+  # Verify the authenticity of the password
+  if not utils.verify(payload.password, user.password):
+    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                        detail="Invalid credentials")
+
+  access_token = oauth2.create_access_token(data={"user_id": user.id});
+
+  return {"token": access_token, "token_type": "bearer"}
+
