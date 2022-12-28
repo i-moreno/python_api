682313c78034941d5ad7addade509d9acb632f65
Carlos <juan.moreno@improving.com>
2022-12-28 15:56:48 -0600

    Clean main.py file

--- app/main.py
+++ app/main.py
@@ -1,29 +1,13 @@
 from fastapi import FastAPI
 
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
-
 from . import models
 from .database import engine
 from .routers import posts, users, auth
 
-app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
-
-# Database connection
-while True:
-    try:
-        connection = psycopg2.connect(host='localhost', database='blog_api',
-                                      user='postgres', password='itexico1', cursor_factory=RealDictCursor)
-        cursor = connection.cursor()
-        print("Database connected successfully")
-        break
-    except Exception as error:
-        print(f"Database connection failed {error}")
-        time.sleep(4)
+app = FastAPI()
 
 app.include_router(posts.router)
 app.include_router(users.router)
