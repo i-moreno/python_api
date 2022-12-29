482dfa32d338239128a8e3790add9bc831c99c31
Carlos <juan.moreno@improving.com>
2022-12-29 13:07:51 -0600

    Enable CORS

--- app/main.py
+++ app/main.py
@@ -1,14 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import models
 from .database import engine
 from .routers import posts, users, votes, auth
 from .config import settings
 
-
-models.Base.metadata.create_all(bind=engine)
+# Since we are using alembic for migrations
+# we can autogenerate tables from models module, so we don't need this command anymore
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# Allowed origins
+origins = ["*"]
+
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(posts.router)
 app.include_router(users.router)
